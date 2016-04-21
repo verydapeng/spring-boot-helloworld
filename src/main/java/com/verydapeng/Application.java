c8cd33c1a862603d3d762ae1750119d1685886a2
Dapeng <verydapeng@gmail.com>
2016-04-21 23:06:12 +0800

    init

---
+++ src/main/java/com/verydapeng/Application.java
@@ -0,0 +1,22 @@
+package com.verydapeng;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@SpringBootApplication
+@RestController
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+	@RequestMapping("/now")
+	String now() {
+		return "now: " + new Date();
+	}
+}
